1a1f37d81f586b38ad9e014516a464f1079a2889
David A Case <david.case@rutgers.edu>
2024-12-21 22:41:34 -0700

    first stab at creating something to send to conda-forge

---
+++ recipes/cpptraj/activate.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export CPPTRAJHOME=${CONDA_PREFIX}
+
---
+++ recipes/cpptraj/build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+arch=`uname`
+
+if [ "$arch" == "Darwin" ]; then
+   ./configure -readline -openblas -arpack -fftw3 -zlib -bzlib -shared clang
+   make install
+else
+   ./configure -readline -openmm -openblas -fftw3 -zlib -bzlib -shared -arpack gnu
+   make install
+   make clean
+   ./configure -readline -openmm -openblas -fftw3 -zlib -bzlib -shared -mpi -arpack gnu
+   make install
+fi
+
+rsync -a README.md config.h LICENSE dat bin lib $PREFIX
+mkdir -p $PREFIX/doc
+rsync -a doc/CpptrajManual.pdf $PREFIX/doc
+
+# Export CPPTRAJHOME automatically
+mkdir -p ${PREFIX}/etc/conda/{activate,deactivate}.d
+cp ${RECIPE_DIR}/activate.sh ${PREFIX}/etc/conda/activate.d/cpptraj.sh
+cp ${RECIPE_DIR}/deactivate.sh ${PREFIX}/etc/conda/deactivate.d/cpptraj.sh
